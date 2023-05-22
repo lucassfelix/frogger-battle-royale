1e776e1ece1da52c03c97295981260d49192494a
Lucas Félix <lsfelix@senacrs.com.br>
2023-05-22 11:48:53 -0300

    Project Setup
    Rider Plugin Download
    .gitignore Hiding Plugin

---
+++ Source/Frogger/Frogger.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Frogger.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Frogger, "Frogger" );
---
+++ Source/Frogger/FroggerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FroggerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FROGGER_API AFroggerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
