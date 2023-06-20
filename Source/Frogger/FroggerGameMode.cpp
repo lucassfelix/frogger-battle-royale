48ab5bbf0436816c96b7428ae455fa6f458f7a49
Lucas Félix <lucas.felix.gamedev@gmail.com>
2023-06-20 17:55:59 -0300

    Connection Restrictions on GameMode
    Reject connection:
     - If match has already started.
     - If game is already full.

--- Source/Frogger/FroggerGameMode.cpp
+++ Source/Frogger/FroggerGameMode.cpp
@@ -3,3 +3,25 @@
 
 #include "FroggerGameMode.h"
 
+bool AFroggerGameMode::ReadyToStartMatch_Implementation()
+{
+	return Super::ReadyToStartMatch_Implementation();
+//	return NumPlayers >= 2;
+}
+
+void AFroggerGameMode::PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId,
+	FString& ErrorMessage)
+{
+	Super::PreLogin(Options, Address, UniqueId, ErrorMessage);
+
+	if(HasMatchStarted())
+	{
+		ErrorMessage = TEXT("Unable to connect to a match in progress!");
+	}
+
+	if(NumPlayers >= MaxPlayers)
+	{
+		ErrorMessage = TEXT("Unable to connect! Match is already full!");
+	}
+	
+}
